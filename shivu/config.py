90b1211df32cd2abac68025d268df0642ed3abe1
Zeta-05 <vjimmy276@gmail.com>
2024-10-28 19:33:38 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "7757912959", "7038202445"
     GROUP_ID = -1002324830715
     TOKEN = "7774006210:AAEdO4XWeHabljjIg2Dm4ZQlWVgkgqqFJFQ"
-    mongo_url = "mongodb+srv://vjimmy276:fCVmNKIqxQ0P54Co@ztx.pxamg.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://Yash_607:Yash_607@cluster0.r3s9sbo.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://graph.org/file/34ce254537a31ca2a788b-f8b59bff546af8dc58.jpg", "https://graph.org/file/0cd48cfd409203a00da74-2a919fe99dd1b238ec.jpg"]
     SUPPORT_CHAT = "https://t.me/Anime_Group_India_Chat"
     UPDATE_CHAT = "https://t.me/Shorekeeper_updates"
